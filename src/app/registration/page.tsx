1dc8efa08ae77121d94da575c6c7e321fc33c0e5
Matheus <matheusmendesreis@outlook.com>
2024-02-19 14:16:34 -0300

    add suspense

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,11 +1,14 @@
 import FormWithStep from "@/components/steps/form-with-multistep";
+import { Suspense } from "react";
 
 export default function Registration() {
   return (
     <>
       <div className="min-h-screen flex items-center justify-center  max-w-[1400px]">
         <section className="w-[540px] min-h-[443px] -mt-24">
-          <FormWithStep />
+          <Suspense fallback={<div>Loading...</div>}>
+            <FormWithStep />
+          </Suspense>
         </section>
       </div>
     </>
